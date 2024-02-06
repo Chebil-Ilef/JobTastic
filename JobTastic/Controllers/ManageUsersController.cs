739277d1a0b563b2a19f7decdeefb309b8b00367
Lost Elf <ilef.chebil@insat.ucar.tn>
2024-02-06 22:08:57 +0100

    manage users part1

---
+++ JobTastic/Controllers/ManageUsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JobTastic.Models;
+using JobTastic.Data;
+using JobTastic.ViewModels;
+
+namespace JobTastic.Controllers
+{
+    public class ManageUsersController : Controller
+    {
+        private readonly AuthDbContext _dbContext;
+
+        public ManageUsersController(AuthDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: ManageUsers
+        public IActionResult Index()
+        {
+            var users = _dbContext.Users.ToList();
+            var viewModel = new ManageUsersViewModel
+            {
+                UsersList = GetUsersList()
+            };
+            return View(viewModel);
+        }
+
+        private List<UserInfo> GetUsersList()
+        {
+            // Fetch user data from the database
+            var usersList = _dbContext.Users
+                .Select(u => new UserInfo
+                {
+                    Name = u.FirstName,
+                    Email = u.Email,
+                    SelectedRole = u.SelectedRole
+                })
+                .ToList();
+
+            return usersList;
+        }
+    }
+}
---
+++ JobTastic/Models/ManageUsersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using JobTastic.Models;
+
+namespace JobTastic.ViewModels
+{
+    public class ManageUsersViewModel
+    {
+        public List<UserInfo> UsersList { get; set; }
+    }
+}
